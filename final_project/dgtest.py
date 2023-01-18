d91a4340c8915161246915b7113e84ade9401bf6
baysideDave <bhca.dave@gmail.com>
2023-01-18 17:23:13 -0500

    Temporary changes5

--- final_project/dgtest.py
+++ final_project/dgtest.py
@@ -1,5 +1,9 @@
 from translator import englishToFrench, frenchToEnglish
-
-
-#print(englishToFrench("Hello"))
 print(englishToFrench(""))
+print(englishToFrench(" "))
+print(englishToFrench("cheese"))
+print(englishToFrench("flat"))
+#
+print(frenchToEnglish(""))
+print(frenchToEnglish(" "))
+print(frenchToEnglish("fromage"))
---
+++ final_project/machinetranslation/dgtest.py
@@ -0,0 +1,10 @@
+from translator import englishToFrench, frenchToEnglish
+#print(englishToFrench(""))
+#print(englishToFrench(" "))
+print(englishToFrench("cheese"))
+print(englishToFrench("flat"))
+#
+#print(frenchToEnglish(""))
+#print(frenchToEnglish(" "))
+print(frenchToEnglish("fromage"))
+print(frenchToEnglish("plate"))
