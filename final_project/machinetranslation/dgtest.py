dd2aa02ebde2e4780b1e35de29913349b2fd6eb3
baysideDave <bhca.dave@gmail.com>
2023-01-19 15:55:13 -0500

    Final changes7

--- final_project/machinetranslation/dgtest.py
+++ final_project/machinetranslation/dgtest.py
@@ -2,9 +2,9 @@ from translator import englishToFrench, frenchToEnglish
 #print(englishToFrench(""))
 #print(englishToFrench(" "))
 print(englishToFrench("cheese"))
-print(englishToFrench("flat"))
+print(englishToFrench("tiger"))
 #
 #print(frenchToEnglish(""))
 #print(frenchToEnglish(" "))
 print(frenchToEnglish("fromage"))
-print(frenchToEnglish("plate"))
+print(frenchToEnglish("auto"))
